0a9fd92d50ac00d88e861fee42c64b6b061cf35e
Jonathan Murmu <jonathanmurmu@gmail.com>
2019-01-20 21:14:58 +0530

    test cases

--- tree/Set1Problem1/set1problem1/tests.py
+++ tree/Set1Problem1/set1problem1/tests.py
@@ -5,6 +5,9 @@ from .demo_data import DemoData
 from .family import Family
 from .generation import Generation
 from .person import Person
+from .family import PERSON_ADDRESS
+from .relationfactory import RelationFactory
+from . import relations
 
 
 class TestGeneration(unittest.TestCase):
@@ -76,14 +79,82 @@ class TestGeneration(unittest.TestCase):
         self.demo.relationship(person_name='Ish', relation='Cousins'), "Ish does not have own cousins.",
         'Incorrect Cousin implementation')
 
-# Run Tests
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestGeneration('test_relationship'))
 
-    return suite
+class TestFamily(unittest.TestCase):
+    def setUp(self):
+        # PERSON_ADDRESS = {}
+        self.person_name = 'Test Person 1'
+        self.test_person_1 = Person(name=self.person_name, gender=MALE)
+        self.test_family = Family(main_member=self.test_person_1)
+    
+    def test_update_address(self):
+        self.assertEqual(
+            self.person_name in PERSON_ADDRESS, True,
+            'Incorrect Update Address implementation')
+        self.assertEqual(
+            "Test Person 2" in PERSON_ADDRESS, False,
+            'Incorrect Update Address implementation')
 
 
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
+class TestPerson(unittest.TestCase):
+    def test_person_class(self):
+        self.person_name = "Test Person"
+        self.gender = MALE
+        self.person_obj = Person(name=self.person_name, gender=self.gender)
+        self.assertEqual(
+            type(self.person_obj), Person,
+            'Incorrect Person class  implementation')
+
+
+class TestRelationFactory(unittest.TestCase):
+    def setUp(self):
+        self.relation_factory = RelationFactory()
+        self.class_instances = {
+            'BrotherInLaw': relations.BrotherInLaw,
+            'Brothers': relations.Brothers,
+            'Children': relations.Children,
+            'Cousins': relations.Cousins,
+            'Daughters': relations.Daughters,
+            'Father': relations.Father,
+            'GrandDaughter': relations.GrandDaughter, 
+            'MaternalAunt': relations.MaternalAunt,
+            'MaternalUncle': relations.MaternalUncle,
+            'Mother': relations.Mother,
+            'PaternalAunt': relations.PaternalAunt,
+            'PaternalUncle': relations.PaternalUncle,
+            'SisterInLaw': relations.SisterInLaw,
+            'Sisters': relations.Sisters,
+            'Sons': relations.Sons,
+        }
+
+    def test_load_relations(self):
+        for cls_name, cls_type in self.class_instances.items():
+            self.assertEqual(
+                self.relation_factory.relation[cls_name], cls_type,
+                'Incorrect load_relation() {0} implementation'.format(cls_name))
+
+
+    def test_create_instance(self):
+
+        wrong_class = "WrongClass"
+        for cls_name, cls_type in self.class_instances.items():
+            self.assertEqual(
+                type(self.relation_factory.create_instance(cls_name)) == cls_type, True,
+                'Incorrect load_relation() MaternalUncle implementation')
+        self.assertEqual(
+            type(self.relation_factory.create_instance(wrong_class)) == relations.NullRelation, True,
+            'Incorrect load_relation() MaternalUncle implementation')
+
+
+
+# # Run Tests
+# def suite():
+#     suite = unittest.TestSuite()
+#     suite.addTest(TestGeneration('test_relationship'))
+
+#     return suite
+
+
+# if __name__ == '__main__':
+#     runner = unittest.TextTestRunner()
+#     runner.run(suite())
