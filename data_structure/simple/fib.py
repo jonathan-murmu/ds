3db0997e9982c6aed4a4b331ce99217d520016ac
Jonathan M <jonathanmurmu@gmail.com>
2022-03-01 22:15:31 +0530

    fibonacci

---
+++ data_structure/simple/fib.py
@@ -0,0 +1,22 @@
+def fib(n):
+    """Fibonacci series of n numbers."""
+    a = 0
+    b = 1
+    sum_of_digits = 0
+    series = []
+    while n > 0:
+        series.append(sum_of_digits)
+        a, b = b, sum_of_digits
+        sum_of_digits = a + b
+
+        n -= 1
+
+    return series
+
+
+print(fib(0))
+print(fib(1))
+print(fib(2))
+print(fib(3))
+print(fib(4))
+print(fib(10))
